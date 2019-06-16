f5e5715dc1161438c8137786ca83614f791b5ebb
Dmitry Patsura <talk@dmtry.me>
2019-06-16 23:19:09 +0300

    Feature(core): Extract serializeParams/transformResponse to helpers

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,13 +1,14 @@
 import {ResponseInterceptorResultEnum, InterceptorsManager, RequestInterceptor, ResponseInterceptor} from './interceptors';
 import {BadResponse} from './errors';
+import {serializeParams, SerializeParamsFn, transformResponse, TransformResponseFn} from "./helpers";
 
 export type ReqsterSettings = {
     headers: {
         [key: string]: string
     },
     timeout: number,
-    serializeParams: (parameters: { [key: string]: any }) => string,
-    transformResponse: (response: ReqsterResponse, url: string, settings: ReqsterRequestDirectSettings) => Promise<any>,
+    serializeParams: SerializeParamsFn,
+    transformResponse: TransformResponseFn,
     validateStatus: (status: number) => boolean,
 };
 
@@ -146,61 +147,14 @@ class Client {
     }
 }
 
-export function serializeParams(parameters: { [key: string]: any }, path: string = ''): string {
-    const result = [];
 
-    for (const k in parameters) {
-        if (parameters[k] !== null) {
-            const value = parameters[k];
-
-            if (Array.isArray(parameters[k])) {
-                value.forEach((v: string) => {
-                    result.push(encodeURIComponent(k) + '[]=' + encodeURIComponent(v));
-                });
-            } else if (typeof value === 'object') {
-                if (path) {
-                    result.push(serializeParams(value, path + '[' + k + ']'));
-                } else {
-                    result.push(serializeParams(value, path + k));
-                }
-            } else {
-                if (path) {
-                    result.push(path + '[' + encodeURIComponent(k) + ']=' + encodeURIComponent(value));
-                } else {
-                    result.push(encodeURIComponent(k) + '=' + encodeURIComponent(value));
-                }
-            }
-        }
-    }
-
-    return result.join('&');
-}
 
 export function getDefaultClientSettings(): ReqsterSettings {
     return {
         headers: {},
         timeout: 0,
         serializeParams,
-        transformResponse: async (response, url, parameters) => {
-            try  {
-                return await response.clone().json()
-            } catch (e) {
-                throw new BadResponse(
-                    'Bad JSON',
-                    {
-                        url,
-                        headers: <{[key: string]: any}>parameters.headers,
-                        method: <string>parameters.method,
-                    },
-                    {
-                        ok: response.ok,
-                        status: response.status,
-                        headers: response.headers,
-                        content: await response.clone().text()
-                    }
-                );
-            }
-        },
+        transformResponse,
         validateStatus: (status) => status >= 200 && status < 300
     };
 }
